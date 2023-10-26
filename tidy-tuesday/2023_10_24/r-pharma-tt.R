200df8be3ed590c7a8bd46d028b010bbf34ad3aa
Benjamin Arancibia <bcarancibia@gmail.com>
2023-10-26 09:50:27 -0400

    data is weird man...found an rstudio problem and had to redownload it...took way to long.

--- tidy-tuesday/2023_10_24/r-pharma-tt.R
+++ tidy-tuesday/2023_10_24/r-pharma-tt.R
@@ -4,3 +4,31 @@ tuesdata <- tidytuesdayR::tt_load('2023-10-24')
 
 
 patient_risk_profiles <- tuesdata$patient_risk_profiles
+
+View(patient_risk_profiles)
+
+#weird thing where sex is both male and female
+patient_risk_profiles %>%
+  select(personId, `Sex = FEMALE`, `Sex = MALE`) %>%
+  filter(`Sex = FEMALE` == 1 & `Sex = MALE` == 1)
+# occurs 9 times.... dropping those observations
+
+colnames(patient_risk_profiles)[21] = "Female"
+colnames(patient_risk_profiles)[22] = "Male"
+
+colnames(patient_risk_profiles)
+
+#test out changing columns
+test <- patient_risk_profiles[!(patient_risk_profiles$Female == 1 & patient_risk_profiles$Male == 1), ] 
+  
+test <- test %>% 
+  mutate(Sex = case_when(`Female` == 1 ~ 'M',
+                         `Male` == 1 ~ 'F')) %>% 
+  select(-Female, -Male)
+
+View(test)
+
+
+
+
+
