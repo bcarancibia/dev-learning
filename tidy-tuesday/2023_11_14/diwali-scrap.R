44e577936e0984797c2e241cf8cb6dc683e8cd4f
Benjamin Arancibia <bcarancibia@gmail.com>
2023-11-16 13:09:09 -0500

    eda of tidy tuesday

--- tidy-tuesday/2023_11_14/diwali-scrap.R
+++ tidy-tuesday/2023_11_14/diwali-scrap.R
@@ -1,9 +1,58 @@
 library(tidytuesdayR)
 library(tidyverse)
+library(ggthemes)
+library(paletteer)
 
 tuesdata <- tidytuesdayR::tt_load('2023-11-14')
 diwali <- tuesdata$diwali_sales_data
 
-diwali <- diwali %>% 
-  select(!(c(Status, unnamed1)))
+theme_ben <- function(){
+  theme_minimal(base_size = 12, base_family = "") %+replace%
+    theme(
+      panel.grid.minor = element_blank(),
+      plot.background = element_rect(fill = 'floralwhite', color = "floralwhite"),
+      
+    )
+}
+#quick EDA
+
+diwali %>% 
+  ggplot(aes(x=Gender)) +
+  geom_histogram(stat='count') +
+  theme_ben()
+
+
+diwali %>% 
+  ggplot(aes(x=`Age Group`)) +
+  geom_histogram(stat = 'count') +
+  theme_ben()
+
+diwali %>% 
+  ggplot(aes(x = Age)) +
+  geom_density() +
+  theme_ben()
+
+##look at state sales
+
+diwali %>% 
+  ggplot(aes(x=State)) +
+  geom_histogram(stat = 'count') + 
+  theme_ben()
+
+diwali %>% 
+  ggplot(aes(x=Occupation)) +
+  geom_histogram(stat = 'count') +
+  theme_ben()
+
+diwali %>% 
+  ggplot(aes(x=Amount)) +
+  geom_density() +
+  theme_ben()
+
+
+#can you predict how much someone will spend?
+
+library(tidymodels)
+library(corrr)
+
 
